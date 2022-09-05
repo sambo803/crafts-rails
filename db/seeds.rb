b1f5f7876f4880d77677e72af9fdfbd5978c4c0e
samuel <sbacon803@gmail.com>
2022-09-05 13:22:52 -0600

    add index action

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+  product = Product.new(name: "Kitty Oven Mitts", price: 15, description: "A cat holds the hot food.", image_url: "https://i.ebayimg.com/images/g/VloAAOSw2UxiISXs/s-l500.jpg")
+  # product.save
+  product = Product.new(name: "Dog mug", price: 8, description: "A dog coffee mug", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-7E4HSZJBF9i5muGjwuQcq7u0Q93HTpw4iA&usqp=CAU")
+  # product.save
+  
+  product = Product.new(name: "leg warmers", price: 20, description: "Fuzzy leg warmers", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTW5GNCcWMSOD5RvgELd5dTny6y6Y2xIZ9hAF7TKH1W-Gw3bxRNBKlAvZKVlntWRaMDAdc&usqp=CAU")
+  # product.save
+
+  
